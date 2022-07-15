b3049b635d014b4b9484640ca44b19fe9a3f9cd7
unknown <caiocbahia@hotmail.com>
2022-07-15 17:33:04 -0300

    Teste com a api do github

--- 11 Axios/src/index.js
+++ 11 Axios/src/index.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-axios.get('https://swapi.dev/api/people/1')
-  .then(res => {
-    console.log(res.data);
-  });
+axios.get('https://api.github.com/users/maykbrito')
+    .then(response => {
+        const user = response.data
+
+        axios.get(user.repos_url)
+            .then(repos => console.log(repos.data))
+    })
+    .catch(err => console.log(err))
