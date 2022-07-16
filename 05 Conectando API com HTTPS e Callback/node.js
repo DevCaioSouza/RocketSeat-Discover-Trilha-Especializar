14c7b7f0188968aa089f24696a41e89970d18e86
unknown <caiocbahia@hotmail.com>
2022-07-15 22:25:14 -0300

    Conectando API com HTTPS e Callback

---
+++ 05 Conectando API com HTTPS e Callback/node.js
@@ -0,0 +1,10 @@
+const https = require('https')
+const API = 'https://jsonplaceholder.typicode.com/users?_limit=2'
+
+https.get(API, res => {
+    console.log(res.statusCode)
+})
+console.log('conectando API')
+
+//o statusCode serve pra ver o código de resposta 
+//da requisição HTTP
