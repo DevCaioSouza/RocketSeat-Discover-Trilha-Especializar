a97c54965234e49b450415f99f34708fc16b5a66
unknown <caiocbahia@hotmail.com>
2022-07-16 22:13:32 -0300

    15 Async Await Completa

--- 15 Async Await/index.js
+++ 15 Async Await/index.js
@@ -3,10 +3,20 @@ const promessa = new Promise(function (resolve, reject) {
 })
 
 async function start(){
-  const result = await promessa
-  console.log(result)
+  try{
+    const result = await promessa
+    console.log(result)
+  } catch(e){
+    console.log(e)
+  } finally {
+    console.log('Rodar sempre!!')
+  }
+
 }
 start()
+
+//O que está acima, substitui o código
+//comentado logo abaixo
 
 // promessa
 //   .then(function(response){
