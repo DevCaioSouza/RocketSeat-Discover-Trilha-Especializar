33c0e711abe89755e4db68e70f2c2435eb09470a
unknown <caiocbahia@hotmail.com>
2022-07-16 22:10:15 -0300

    segunda estrutura da aula

--- 15 Async Await/index.js
+++ 15 Async Await/index.js
@@ -2,13 +2,19 @@ const promessa = new Promise(function (resolve, reject) {
   return resolve('Ok')
 })
 
-promessa
-  .then(function(response){
-    console.log(response)
-  })
-  .catch(function(error){
-    console.log(error)
-  })
-  .finally(function(){
-    console.log('Sempre irei executar')
-  })
+async function start(){
+  const result = await promessa
+  console.log(result)
+}
+start()
+
+// promessa
+//   .then(function(response){
+//     console.log(response)
+//   })
+//   .catch(function(error){
+//     console.log(error)
+//   })
+//   .finally(function(){
+//     console.log('Sempre irei executar')
+//   })
