cb47b4f98aa2776d74651f2b027dd39296f253db
unknown <caiocbahia@hotmail.com>
2022-07-17 22:56:54 -0300

    testes com Promise.all - Pegando múltiplos dados

--- 01 JavaScript Assíncrono e Promises/13 Executando Promessas em paralelo com Promise all/src/index.js
+++ 01 JavaScript Assíncrono e Promises/13 Executando Promessas em paralelo com Promise all/src/index.js
@@ -7,13 +7,17 @@
 
 import axios from "axios";
 
+
 Promise.all([
+    axios.get('https://api.github.com/users'),
     axios.get('https://api.github.com/users/maykbrito'),
     axios.get('https://api.github.com/users/maykbrito/repos')
 ])
 .then(responses => {
-    console.log(responses[0].data.login)
-    console.log(responses[1].data.length)
+    console.log(responses[0].data[0])
+    console.log(responses[0].data[1])
+    console.log(responses[1].data.login)
+    console.log(responses[2].data.length)
 })
 .catch(err => console.log(err.message))
 
