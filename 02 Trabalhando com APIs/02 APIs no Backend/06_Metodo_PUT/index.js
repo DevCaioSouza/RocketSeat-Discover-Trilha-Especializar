e2dfaaf4d0f25435214b2a739785f689659d2fa3
unknown <caiocbahia@hotmail.com>
2022-07-20 20:57:27 -0300

    Refinando o código e finalizando

--- 02 Trabalhando com APIs/02 APIs no Backend/06_Metodo_PUT/index.js
+++ 02 Trabalhando com APIs/02 APIs no Backend/06_Metodo_PUT/index.js
@@ -12,6 +12,7 @@ app.use(express.json())
 app.route('/').get((req, res) => res.send(author))
 
 app.route('/').put((req, res) => {
-  author = req.body
+  author = req.body.author
+  //apenas req.body mandaria junto as chaves {} do objeto JSON
   res.send(author)
 })
