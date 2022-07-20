26c675cd4b962dd1217652ec33c03a047415038f
unknown <caiocbahia@hotmail.com>
2022-07-20 10:25:59 -0300

    Enviando dados pro insomnia

--- 02 Trabalhando com APIs/02 APIs no Backend/rascunhos_DUMP/testes_com_POST/index.js
+++ 02 Trabalhando com APIs/02 APIs no Backend/rascunhos_DUMP/testes_com_POST/index.js
@@ -6,4 +6,8 @@ app.listen(3000)
 
 app.use(express.json())
 
-app.route("/").post((req, res) => console.log(req.body))
+//mostrando apenas a resposta no console \/
+//app.route("/").post((req, res) => console.log(req.body))
+
+//enviando o corpo da requisição para o INSOMNIA
+app.route("/").post((req, res) => res.send(req.body))
