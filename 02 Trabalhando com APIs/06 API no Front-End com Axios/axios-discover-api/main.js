041b4cec81ca7876dd06384dd4311798be107dcf
unknown <caiocbahia@hotmail.com>
2022-07-22 00:30:55 -0300

    Retornando em JSON.stringify no browser

--- 02 Trabalhando com APIs/06 API no Front-End com Axios/axios-discover-api/main.js
+++ 02 Trabalhando com APIs/06 API no Front-End com Axios/axios-discover-api/main.js
@@ -1,14 +1,15 @@
 const url = "http://localhost:5500/api"
 
-function getUsers(){
+function getUsers() {
   axios.get(url)
-  .then(response => {
-    console.log(response)
-  })
-  .catch(error => console.error(error))
+    .then(response => {
+      apiResult.textContent = JSON.stringify(response.data)
+    })
+    .catch(error => console.error(error))
 }
 
 getUsers()
 
-
 //console.error é pra ficar destacado de vermelho
+
+// o data do response.data é pra ir direto no users
